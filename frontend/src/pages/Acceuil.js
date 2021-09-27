cfc2805bfca4d8d6f133788772275d6b02539a14
Jean Chalret <jecdr_75@hotmail.fr>
2021-09-27 11:14:06 +0200

    page posts

--- frontend/src/pages/Acceuil.js
+++ frontend/src/pages/Acceuil.js
@@ -11,28 +11,25 @@ const Acceuil = () => {
       </header>
       <main>
         <div className="acceuil-container">
-          <h1>Acceuil</h1>
+          {/* <h1>Acceuil</h1> */}
           <br />
           <div className="posts-container">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-              error rerum dicta, non reiciendis totam provident dignissimos
-              laudantium earum dolore iusto. Corporis esse maxime recusandae ad
-              repellat. Ipsa, laboriosam. Ea expedita tempora fuga, in veniam
-              animi, vitae placeat numquam quasi reiciendis cum earum temporibus
-              commodi est odit minima exercitationem totam unde autem. Facere
-              est culpa, fugit ex iste quisquam aliquam temporibus veniam
-              incidunt. Voluptatem, mollitia ea vitae adipisci ipsa perferendis
-              maiores earum sint autem nam alias, consequuntur laborum cumque
-              nostrum nobis fugiat enim odio eligendi dolore rerum accusantium
-              labore asperiores. Recusandae tenetur sapiente tempore est, enim
-              hic consequatur mollitia eaque quisquam consectetur adipisci
-              consequuntur animi impedit quos eum, possimus vel velit! Tempora,
-              beatae recusandae. Facilis distinctio explicabo aperiam quae
-              veniam architecto? Praesentium sunt, iusto maiores iure aliquam
-              tempora aperiam at laboriosam unde consequuntur labore adipisci
-              asperiores necessitatibus ut porro iste!
-            </p>
+            <form>
+              <textarea
+                type="text"
+                placeholder="Quoi de neuf ?"
+                className="post"
+              />
+              <div>
+                <input type="submit" value="Publier" className="submitPost" />
+                <i className="fas fa-image" title="ajouter une image"></i>
+                <input
+                  type="file"
+                  className="imagePost"
+                  title="ajouter une image"
+                />
+              </div>
+            </form>
           </div>
         </div>
       </main>
